d718b4655d3b456b558845cdcf82a5aa623c7fe6
Laudes Centurion <laudes.centurionrivas@tuni.fi>
2023-04-13 20:36:38 +0300

    PatientListView

--- frontend/src/components/PatientListView.js
+++ frontend/src/components/PatientListView.js
@@ -1,34 +1,62 @@
 import React, { useEffect, useState } from 'react';
 
 const PatientList = ({ patients, setPatients }) => {
+  const [searchTerm, setSearchTerm] = useState('');
 
-  console.log(patients)
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredPatients = patients.filter(
+    (patient) =>
+      patient.resource.name[0].given
+        .join(' ')
+        .toLowerCase()
+        .includes(searchTerm.toLowerCase()) ||
+      patient.resource.name[0].family[0]
+        .toLowerCase()
+        .includes(searchTerm.toLowerCase()) ||
+      patient.resource.id.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div>
       <h1>Patient List</h1>
+      <input
+        type="search"
+        class="search"
+        placeholder="Search patients..."
+        value={searchTerm}
+        onChange={handleSearch}
+      />
       <table>
         <thead>
           <tr>
+            <th>Patient ID</th>
             <th>Name</th>
-            <th>Family Name</th>
+            <th>Last Name</th>
           </tr>
         </thead>
         <tbody>
-          {patients ?
-          patients.map(patient => (
-            <tr key={patient.id}>
-              <td>{patient.resource.name[0].given.map(name => <p> {name} </p>)}</td>
-              <td>{patient.resource.name[0].family[0]}</td>
+          {filteredPatients.length > 0 ? (
+            filteredPatients.map((patient) => (
+              <tr key={patient.id}>
+                <td>{patient.resource.id}</td>
+                <td>{patient.resource.name[0].given.join(' ')}</td>
+                <td>{patient.resource.name[0].family[0]}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="3">No matching patients found</td>
             </tr>
-          )):
-            <p>Loading</p> }
+          )}
         </tbody>
       </table>
     </div>
   );
-}
-
-/* Do a function for ordering and filtering 
+};
 
 export default PatientList;
+
+
